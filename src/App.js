79f98b433b8e10920596422740bd88314620ca1f
DavidCatch <raihanwibyanto@gmail.com>
2022-11-28 22:41:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import ParticlesBg from 'particles-bg';
+import { BsInstagram, BsGithub, BsYoutube } from 'react-icons/bs';
+import { FaTiktok } from 'react-icons/fa';
+import Profile from './img/Profile.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card">
+        <div className="card_body">
+          <div className="profile text-center">
+            <img
+              src={Profile}
+              alt="Profile"
+              className="avatar"
+            />
+            <div className="bg_content rd_12 p_8">
+              <h1>Raihan</h1>
+              <p>Student</p>
+              <p>SMK Media Informatika Jakarta</p>
+            </div>
+          </div>
+          <div className="mt-16">
+            <a
+              href="https://www.instagram.com/official_playergames/?hl=id"
+              className="btn_action bg_content"
+            >
+              <BsInstagram />
+              <span>Instagram</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              href="https://www.tiktok.com/@siapa_diriku08"
+              className="btn_action bg_content"
+            >
+              <FaTiktok />
+              <span>Tiktok</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              href="https://www.youtube.com/channel/UC1x2pY3Dj2X5YHHOW_gt3-g"
+              className="btn_action bg_content"
+            >
+              <BsYoutube />
+              <span>Youtube</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              href="https://github.com/Davidcatch?tab=repositories"
+              className="btn_action bg_content"
+            >
+              <BsGithub />
+              <span>GitHub</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <ParticlesBg
+        type="thick"
+        bg={true}
+      />
     </div>
   );
 }
